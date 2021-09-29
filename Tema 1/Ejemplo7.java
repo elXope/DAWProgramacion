71b3db22bcad2c85c5e9655ffdad8b902a5fbfc4
elXope <errchosep25@gmail.com>
2021-09-29 12:24:40 +0200

    Acabat ex 7

--- Tema 1/Ejemplo7.java
+++ Tema 1/Ejemplo7.java
@@ -3,19 +3,22 @@ public class Ejemplo7 {
     public static void main(String[] args) {
         Scanner llig = new Scanner(System.in);
         boolean incorrecte = true;
+        float preu = 0;
+        float preuRebaixat = 0;
+        // No sé perquè no me dixa usar-los sense inicialitzar-los.
 
         while(incorrecte){
-        System.out.println("Introduix el preu inicial: ");
-        final float preu = llig.nextFloat();
+            System.out.println("Introduix el preu inicial: ");
+            preu = llig.nextFloat();
 
-        System.out.println("Introduix el preu rebaixat: ");
-        final float preuRebaixat = llig.nextFloat();
+            System.out.println("Introduix el preu rebaixat: ");
+            preuRebaixat = llig.nextFloat();
 
-        if(preu < preuRebaixat){
-            System.out.println("El preu rebaixat no pot ser major que el original.");
-        }else{
-            incorrecte = false;
-        }
+            if(preu < preuRebaixat){
+                System.out.println("El preu rebaixat no pot ser major que el original.");
+            }else{
+                incorrecte = false;
+            }
         }
 
         final float rebaixa = (preu - preuRebaixat)/preu * 100;
