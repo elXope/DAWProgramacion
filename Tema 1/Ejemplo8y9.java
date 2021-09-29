eba27e85219c4d2f90a02fd6ba8479cc9f2f613c
elXope <errchosep25@gmail.com>
2021-09-29 12:36:16 +0200

    He fet el 8 i el 9 junts sense voler

---
+++ Tema 1/Ejemplo8y9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejemplo8y9 {
+    public static void main(String[] args) {
+        Scanner llig = new Scanner(System.in);
+
+        System.out.println("Introduix la teua edat: ");
+        final byte edat = llig.nextByte();
+
+        if(edat >= 18){
+            System.out.println("Tens la edat legal.");
+        }else{
+            System.out.println("No tens la edat legal.");
+        }
+    }
+}
