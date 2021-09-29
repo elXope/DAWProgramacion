c2206f719a5231ffe7ce2568b7815afc85e62cdb
elXope <errchosep25@gmail.com>
2021-09-29 10:29:20 +0200

    Fet ex6

--- Tema 1/Ejemplo6.java
+++ Tema 1/Ejemplo6.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class Ejemplo6 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
+        Scanner llig = new Scanner(System.in);
+
+        System.out.println("Escriu el radi (m): ");
+        final double radi = llig.nextFloat();
+
+        final double perimetre = 2*Math.PI*radi;
+        final double area = Math.PI*Math.pow(radi, 2);
+
+        System.out.println("El perímetre és de " + perimetre + "m i l'àrea de " + area + "m*m.");
     }
 }
