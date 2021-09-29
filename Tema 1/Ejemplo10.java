a6ff0b03d2e62f832cd1578ef8a23e35a18fe5ae
elXope <errchosep25@gmail.com>
2021-09-29 12:44:24 +0200

    Fet el 10 mestic cansant dels commit

---
+++ Tema 1/Ejemplo10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejemplo10 {
+    public static void main(String[] args) {
+        Scanner llig = new Scanner(System.in);
+
+        System.out.println("Introduix un número: ");
+        final float num = llig.nextFloat();
+
+        if(num < 0){
+            System.out.println("El número introduït és negatiu.");
+        }else{
+            System.out.println("El número introduït és positiu.");
+        }
+    }
+}
