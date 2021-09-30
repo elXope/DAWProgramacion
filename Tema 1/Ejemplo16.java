7e36d5295acb4037b5038f7ab398c2d18989626d
elXope <errchosep25@gmail.com>
2021-09-30 13:50:16 +0200

    Començat el 16

---
+++ Tema 1/Ejemplo16.java
@@ -0,0 +1,8 @@
+public class Ejemplo16 {
+    public static void main(String[] args) {
+        Scanner llig = new Scanner(System.in);
+
+        System.out.println("Introduix una operació: ");
+        String operacio = llig.next();
+    }
+}
