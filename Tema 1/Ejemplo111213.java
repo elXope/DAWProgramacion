1281abf8fdd7de5a805fa46d64066a23bc1dbbf7
elXope <errchosep25@gmail.com>
2021-09-30 12:53:01 +0200

    Acabat fusió exercicis 11, 12 i 13.

--- Tema 1/Ejemplo111213.java
+++ Tema 1/Ejemplo111213.java
@@ -14,33 +14,44 @@ public class Ejemplo111213 {
         }
         Arrays.sort(nums);
 
-        //while MHE KEDAT ACÍ
+        boolean seguir = true;
+        while(seguir){
 
-        System.out.println("Què vols fer amb els números?\n1 --> Ordenar de forma ascendent.\n2 --> Ordenar de forma descendent.\n3 --> Mostrar valor més gran.");
+            System.out.println("Què vols fer amb els números?\n1 --> Ordenar de forma ascendent.\n2 --> Ordenar de forma descendent.\n3 --> Mostrar valor més gran.\n4 --> Mostrar valor més xicotet.\n5 --> Finalitzar programa.");
 
-        switch(llig.nextInt()){
-            case 1:
-                for(int i = 0; i < nums.length - 1; i++){
-                    System.out.print(nums[i] + ", ");
-                }
-                System.out.print(nums[nums.length - 1] + ".\n");
-                break;
+            switch(llig.nextInt()){
+                case 1:
+                    for(int i = 0; i < nums.length - 1; i++){
+                        System.out.print(nums[i] + ", ");
+                    }
+                    System.out.print(nums[nums.length - 1] + ".\n\n");
+                    break;
 
-            case 2:
-                for(int i = nums.length - 1; i > 0; i--){
-                    System.out.print(nums[i] + ", ");
-                }
-                System.out.print(nums[0] + ".\n");
-                break;
+                case 2:
+                    for(int i = nums.length - 1; i > 0; i--){
+                        System.out.print(nums[i] + ", ");
+                    }
+                    System.out.print(nums[0] + ".\n\n");
+                    break;
 
-            case 3:
-                System.out.print(nums[nums.length - 1] + ".\n");
-                break;
+                case 3:
+                    System.out.print(nums[nums.length - 1] + ".\n\n");
+                    break;
 
-            default:
-                System.out.println("El número introduït no és vàlid.");
-                break;
+                case 4:
+                    System.out.print(nums[0] + ".\n\n");
+                    break;
+                    
+                case 5:
+                    seguir = false;
+                    break;
+
+                default:
+                    System.out.println("El número introduït no és vàlid.\n");
+                    break;
+            }
         }
+        System.out.println("Programa finalitzat.");
 
     }
 }
