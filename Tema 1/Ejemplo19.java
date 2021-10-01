923abc2ee0fb4f0cfd30b8be8db1faa4bedba6db
elXope <errchosep25@gmail.com>
2021-10-01 12:36:36 +0200

    Fent el 17

--- Tema 1/Ejemplo19.java
+++ Tema 1/Ejemplo19.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 public class Ejemplo19 {
     public static void main(String[] args) {
         Scanner llig = new Scanner(System.in);
-        final byte eurosh;
-        byte hores;
-        int pagoBrut, souNet, impostos;
+        final short eurosh, hores;
+        float pagoBrut, souNet, impostos;
 
         System.out.println("Introduix els euros/hora:");
         eurosh = llig.nextByte();
         System.out.println("Introduix les hores:");
         hores = llig.nextByte();
 
-        while(hores < 0){
-            
+        if (hores > 35) {
+            pagoBrut = 35*eurosh + (float)1.5*eurosh*(hores - 35);  
+        } else {
+            pagoBrut = 35*eurosh;
         }
         
+        if (pagoBrut > 900) {
+            impostos = 400*(float).25 + (pagoBrut - 900)*(float).45;
+        } else if (pagoBrut > 500) {
+            impostos = pagoBrut;
+        }
 
     }
 }
