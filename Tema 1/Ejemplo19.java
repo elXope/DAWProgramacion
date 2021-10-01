45ccac09d4f8b2a7f47948166b8bc28302668014
elXope <errchosep25@gmail.com>
2021-10-01 12:10:10 +0200

    na començant a plantejar el 19

---
+++ Tema 1/Ejemplo19.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejemplo19 {
+    public static void main(String[] args) {
+        Scanner llig = new Scanner(System.in);
+        final byte eurosh;
+        byte hores;
+        int pagoBrut, souNet, impostos;
+
+        System.out.println("Introduix els euros/hora:");
+        eurosh = llig.nextByte();
+        System.out.println("Introduix les hores:");
+        hores = llig.nextByte();
+
+        while(hores < 0){
+            
+        }
+        
+
+    }
+}
