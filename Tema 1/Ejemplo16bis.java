55cfdcb5e11e2be8eaec471b69d4107776459966
elXope <errchosep25@gmail.com>
2021-10-01 10:01:12 +0200

    Acabat ex 16

---
+++ Tema 1/Ejemplo16bis.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejemplo16bis {
+    public static void main(String[] args) {
+        Scanner llig = new Scanner(System.in);
+        final double num1, num2;
+        final String operacio;
+
+        System.out.println("Introduix la operació que vols fer (+,-,*,/): ");
+        operacio = llig.next();
+        System.out.println("Introduix el primer número: ");
+        num1 = llig.nextDouble();
+        System.out.println("Introduix el segon número: ");
+        num2 = llig.nextDouble();
+
+        if(operacio.contains("+")){
+            System.out.println(num1 + num2);
+        }else if(operacio.contains("-")){
+            System.out.println(num1 - num2);
+        }else if(operacio.contains("*")){
+            System.out.println(num1 * num2);
+        }else if(operacio.contains("/")){
+            System.out.println(num1 / num2);
+        }else{
+            System.out.println("La operació no és vàlida.");
+        }
+    }
+}
