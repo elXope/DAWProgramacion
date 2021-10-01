6533b803ce27d6fa6ae83dd5439efab2971fff91
elXope <errchosep25@gmail.com>
2021-10-01 09:36:01 +0200

    Acabat ex16 v1

--- Tema 1/Ejemplo16.java
+++ Tema 1/Ejemplo16.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Ejemplo16 {
     public static void main(String[] args) {
         Scanner llig = new Scanner(System.in);
 
-        System.out.println("Introduix una operació: ");
-        String operacio = llig.next();
+        System.out.println("Introduix quina operació vols fer.\nHa de ser de l'estil 'a + b' i pots usar un d'estos quatre operadors: +, -, * o /.\nNOMÉS NUMEROS ENTERS. Divisió entera.");
+        String operacio = llig.nextLine();
+        operacio = operacio.replace(" ","");
+        String[] numsStr;
+
+        if(operacio.contains("+")){
+            //
+            numsStr = operacio.split("\\+");
+            System.out.println(Integer.parseInt(numsStr[0]) + Integer.parseInt(numsStr[1]));
+        }else if(operacio.contains("-")){
+            numsStr = operacio.split("-");
+            System.out.println(Integer.parseInt(numsStr[0]) - Integer.parseInt(numsStr[1]));
+        }else if(operacio.contains("*")){
+            numsStr = operacio.split("\\*");
+            System.out.println(Integer.parseInt(numsStr[0]) * Integer.parseInt(numsStr[1]));
+        }else if(operacio.contains("/")){
+            numsStr = operacio.split("\\/");
+            System.out.println(Integer.parseInt(numsStr[0]) / Integer.parseInt(numsStr[1]));
+        }else{
+            System.out.println("Operació no vàlida.");
+        }
+        
     }
 }
