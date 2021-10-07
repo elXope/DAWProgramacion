2c9a64654330c810bf0a14f9ea9299a2f388ff74
elXope <errchosep25@gmail.com>
2021-10-07 11:45:18 +0200

    He afegit la porta XOR a Porta

---
+++ Proves/provetes2.java
@@ -0,0 +1,23 @@
+public class provetes2 {
+    public static void main(String[] args) {
+        Porta[] llista = new Porta[3];
+        llista[0] = new Porta(3);
+        llista[1] = new Porta(3);
+        llista[2] = new Porta(3);
+
+        byte[][] plantilla = new byte[3][3];
+        plantilla[0][0] = 3; //
+        plantilla[0][1] = 0;
+        plantilla[0][2] = 1;
+        plantilla[1][0] = 4;
+        plantilla[1][1] = 1;
+        plantilla[1][2] = 2;
+        plantilla[2][0] = 5;
+        plantilla[2][1] = 3;
+        plantilla[2][2] = 4;
+
+        Circuit circo = new Circuit(llista, plantilla);
+        circo.taulaVeritat();
+        System.out.println(circo.solucio.length);
+    }
+}
