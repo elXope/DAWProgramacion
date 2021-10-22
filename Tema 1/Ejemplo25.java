f43d1df929728b4e012cc8b321c927c8eebecebe
elXope <errchosep25@gmail.com>
2021-10-22 09:15:52 +0200

    Millorat Ej25

--- Tema 1/Ejemplo25.java
+++ Tema 1/Ejemplo25.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Ejemplo25 {
     public static void main(String[] args) {
         Scanner llig = new Scanner(System.in);
-        System.out.println("Introduix un número: ");
+        System.out.println("Introduix un número enter: ");
         long factorial = llig.nextLong();
         llig.close();
 
+        if (factorial < 0) return;
         for (long i = factorial - 1; i > 0; i--){
-            factorial = factorial*i;
+            factorial *= i;
         }
 
         System.out.println("Factorial: " + factorial);
