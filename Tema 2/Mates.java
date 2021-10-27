f3ae4e923f40a417ef200830061e95f835433356
elXope <errchosep25@gmail.com>
2021-10-27 10:25:47 +0200

    Afegida funció de l'àrea d'un cercle

--- Tema 2/Mates.java
+++ Tema 2/Mates.java
@@ -14,4 +14,8 @@ public class Mates {
     public static double division(double d1, double d2){
         return d1 / d2;
     }
+
+    public static double areaCercle(double radi){
+        return Math.PI * radi * radi;
+    }
 }
