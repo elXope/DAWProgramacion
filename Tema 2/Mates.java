f58e262ec4c0d9704b8a089df94227d4106c185d
elXope <errchosep25@gmail.com>
2021-10-27 10:20:25 +0200

    Mates.java sense main

--- Tema 2/Mates.java
+++ Tema 2/Mates.java
@@ -1,12 +1,4 @@
 public class Mates {
-    public static void main(String[] args) {
-        double a = 10.5, b = 5.25;
-        System.out.println(suma(a, b));
-        System.out.println(resta(a, b));
-        System.out.println(multiplicacion(a, b));
-        System.out.println(division(a, b));
-    }
-
     public static double suma(double d1, double d2){
         return d1 + d2;
     } 
