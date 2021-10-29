f0416f0cffa0a1fc52d751202fded3c4987e93a4
elXope <errchosep25@gmail.com>
2021-10-29 09:47:19 +0200

    Acabat el getInt i el getDouble no me va

---
+++ Tema 2/Entrada.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Entrada {
+    public static int getInteger(){
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Introduix un número enter: ");
+        while (!lector.hasNextInt()){
+            lector.next();
+            System.out.println("Això no és un enter. Introduix un número enter: ");
+        }
+        int enter = lector.nextInt();
+        lector.close();
+        return enter;
+    }
+
+    public static double getDouble(){
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Introduix un double: ");
+        while (!lector.hasNextDouble()){
+            lector.next();
+            System.out.println("Això no és un double. Introduix un double: ");
+        }
+        double d = lector.nextDouble();
+        lector.close();
+        return d;
+    }
+
+    public static int getInteger2(){
+        Scanner lector = new Scanner(System.in);
+        int enter = 0;
+        System.out.println("Introduix un número enter: ");
+        if (!lector.hasNextInt()){
+            System.out.println("Això no és un enter.");
+            enter = getInteger();
+        }else{
+            enter = lector.nextInt();
+        }
+        return enter;
+    }
+
+}
---
+++ Tema 2/ProvesEntrada.java
@@ -0,0 +1,9 @@
+public class ProvesEntrada {
+    public static void main(String[] args) {
+        int a = Entrada.getInteger();
+        System.out.println("L'enter introduit és: " + a);
+
+        double d = Entrada.getDouble();
+        System.out.println("El double introduit és: " + a);
+    }
+}
