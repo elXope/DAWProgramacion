5a99207a8ab042e243767bf1d66e38cbed69c4ff
elXope <errchosep25@gmail.com>
2021-11-05 08:39:25 +0100

    Primer exercici

---
+++ AceptaElReto/P116HolaMundo.java
@@ -0,0 +1,11 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+public class P116HolaMundo {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int max = lector.nextInt();
+        for(int i = 0; i < max; i++) System.out.println("Hola mundo.");
+    }
+}
