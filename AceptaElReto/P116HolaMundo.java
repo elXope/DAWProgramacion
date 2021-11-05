fb1362da11fd1e350263ee9dc220c6bcd66c22bc
elXope <errchosep25@gmail.com>
2021-11-05 09:00:20 +0100

    En byte

--- AceptaElReto/P116HolaMundo.java
+++ AceptaElReto/P116HolaMundo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class P116HolaMundo {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        int max = lector.nextInt();
-        for(int i = 0; i < max; i++) System.out.println("Hola mundo.");
+        byte max = lector.nextByte();
+        for(byte i = 0; i < max; i++) System.out.println("Hola mundo.");
     }
 }
