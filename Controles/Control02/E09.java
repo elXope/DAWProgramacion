c38c8284d2b3bc9470b9dcfebcba3ff5832a4a07
elXope <errchosep25@gmail.com>
2021-11-08 08:14:31 +0100

    Acabat exercici 9.

--- Controles/Control02/E09.java
+++ Controles/Control02/E09.java
@@ -1,5 +1,23 @@
 package Control02;
 
-public class E09 {
-    
+import java.util.Scanner;
+
+public class E09 { 
+    public static void main(String[] args) {
+        int c1, c2, nl;
+        int nf = 60;
+        Scanner lector = new Scanner(System.in);
+        System.out.print("Ingrese nota certamen 1: ");
+        c1 = lector.nextInt();
+        System.out.print("Ingrese nota certamen 2: ");
+        c2 = lector.nextInt();
+        System.out.print("Ingrese nota laboratorio: ");
+        nl = lector.nextInt();
+        lector.close();
+        System.out.println("Necesita nota " + notaC3(c1, c2, nl, nf) + " en el certamen 3.");
+    }
+
+    private static double notaC3(int c1, int c2, int nl, int nf){
+        return 3*(nf - nl*0.3)/0.7 - c1 - c2;
+    }
 }
