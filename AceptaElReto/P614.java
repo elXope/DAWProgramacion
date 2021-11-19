62206bcf9140b6e0ef63982ee7c27b32fe06abcd
elXope <errchosep25@gmail.com>
2021-11-19 12:43:13 +0100

    Acabat p614

--- AceptaElReto/P614.java
+++ AceptaElReto/P614.java
@@ -1,39 +1,43 @@
 package AceptaElReto;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class P614 {
     public static void main(String[] args) {
         Scanner lec = new Scanner(System.in);
-        final int T = lec.nextInt();
-        final int E = lec.nextInt();
-        int[] pos = new int[E];
-        for (int i = 0; i < E; i++) {
-            pos[i] = lec.nextInt();
+        ArrayList<Integer> output = new ArrayList<>();
+        int T, E, ct, ft;
+        int[] pos;
+        while(true){
+            T = lec.nextInt();
+            E = lec.nextInt();
+            if (T == 0 && E == 0) break;
+            pos = new int[E];
+            for (int i = 0; i < E; i++) {
+                pos[i] = lec.nextInt();
+            }
+            ct = E/2 - T/2;
+            ft = E/2 + T/2;
+            if(E%2 == 0 && T%2 == 0) ft --;
+            if(E%2 != 0 || T%2 != 0){
+                if(T%2 != 0){
+                    ct--;
+                }
+                if (pos[ct] > pos[ft]) ft--;
+                else ct++;
+            }
+            output.add(calcula(ct, ft, pos));
         }
-        long sum = 0;
-        if(T%2 == 0 && E%2 != 0) {
-            int ct = E/2 - T/2;
-            int ft = E/2 + T/2;
-            if (pos[ct] > pos[ft]) ft--;
-            else ct++;
-            sum = calcula(ct, ft, pos);
-        } else if(E%2 == 0 && T%2 != 0) {
-            int ct = E/2 - (T/2 + 1);
-            int ft = E/2 + T/2;
-            if (pos[ct] > pos[ft]) ft--;
-            else ct++;
-            sum = calcula(ct, ft, pos);
-        } else {
-            int ct = E/2 - T/2;
-            int ft = E/2 + T/2;
-            sum = calcula(ct, ft, pos);
+        lec.close();
+
+        for(int i = 0; i < output.size(); i++){
+            System.out.println(output.get(i));
         }
-        System.out.println(sum);
     }
 
-    public static long calcula(int ct, int ft, int[] pos){
-        long sum = 0;
+    public static int calcula(int ct, int ft, int[] pos){
+        int sum = 0;
         for(int i = 0; i < ct; i++){
             sum += pos[i] * (ct - i);
         }
@@ -41,5 +45,5 @@ public class P614 {
             sum += pos[i] * (i - ft);
         }
         return sum;
-    }    
+    }
 }
