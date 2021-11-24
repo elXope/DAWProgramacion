fdc082f9d6ac8cc6f5193c09a5c5f8045c8572c1
elXope <errchosep25@gmail.com>
2021-11-24 08:54:05 +0100

    Primer exercici t3

---
+++ Tema 3/Test.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import Arreglo;
+
+public class Test {
+    public static void main(String[] args) {
+        // prova Eliminar
+        int[] prova = {0, 1, 2, 3};
+        prova = Arreglo.eliminar(prova);
+    }
+}
