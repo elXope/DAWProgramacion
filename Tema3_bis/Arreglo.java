adaade706259d40b5918cbf469c560f9a669f235
elXope <errchosep25@gmail.com>
2021-11-26 10:46:54 +0100

    He fet fatal lo dels commits

--- Tema3_bis/Arreglo.java
+++ Tema3_bis/Arreglo.java
@@ -22,8 +22,8 @@ public class Arreglo {
         return output;
     }
 
-    public static boolean[][] eliminarFilaBoolean(boolean[][] mat, int fila){
-        boolean[][] output = new boolean[mat.length - 1][mat[0].length];
+    public static int[][] eliminarFila(int[][] mat, int fila){
+        int[][] output = new int[mat.length - 1][mat[0].length];
         int j = 0;
         for (int i = 0; i < output.length; i++){
             if (i == fila) j++;
@@ -36,11 +36,10 @@ public class Arreglo {
     public static int[] eliminarDuplicados(int[] arreglo){
         int[] copia = Arrays.copyOf(arreglo, arreglo.length);
         Arrays.sort(copia);
-        int cont = 0;
+
         for (int i = 0; i < copia.length - 1; i++){
             if(copia[i] == copia[i+1]){
-                copia = eliminar(copia,i);
-                i--;
+                copia = eliminar(copia,i--);
             }
         }
         return copia;
@@ -52,10 +51,90 @@ public class Arreglo {
 
         for (int i = 0; i < copia.length - 1; i++){
             if(copia[i] != copia[i+1]){
-                copia = eliminar(copia,i);
-                i--;
+                copia = eliminar(copia,i--);
             }
         }
         return eliminarDuplicados(copia);
+    }
+
+    public static int[] comunes(int[] a1, int[] a2) {
+        a1 = eliminarDuplicados(a1);
+        a2 = eliminarDuplicados(a2);
+        int length = a1.length > a2.length ? a1.length : a2.length;
+        int[] output = new int[length];
+        int indOut = 0;
+        for (int i = 0; i < a1.length; i++){
+            for (int j = 0; j < a2.length; j++){
+                if (a1[i] == a2[j]){
+                    output[indOut++] = a1[i];
+                }
+            }
+        }
+        return Arrays.copyOf(output,indOut);
+    }
+
+    public static int[][] sumaIgual(int[] arreglo, int num){
+        int stop = arreglo.length;
+        int indOut = 0;
+        int [][] output = new int[arreglo.length][2];
+        int[] copia = Arrays.copyOf(arreglo,arreglo.length);
+        Arrays.sort(copia);
+        for(int i = 0; i < stop; i++){
+            for(int j = i+1; j < stop; j++){
+                if (copia[i] + copia[j] == num){
+                    stop = j;
+                    output[indOut][0] = copia[i];
+                    output[indOut][1] = copia[j];
+                    indOut++;
+                } else if(copia[i] + copia[j] > num){
+                    stop = j;
+                }
+            }
+        }
+        return Arrays.copyOf(output,indOut);
+    }
+
+    public static boolean iguales(int[] a1, int[] a2){
+        for (int i = 0; i < a1.length; i++){
+            if (a1[i] != a2[i]) return false;
+        }
+        return true;
+    }
+
+    public static int[] paresImpares(int[] arreglo){
+        int[] output = new int[2]; //output[0] = pares, output[1] = impares
+        for (int i = 0; i < arreglo.length; i++){
+            if (arreglo[i] % 2 == 0) output[0]++;
+            else output[1]++;
+        }
+        return output;
+    }
+
+    public static void moverCeros(int[] arreglo){
+        int ultimaPos = arreglo.length - 1;
+        for(int i = 0; i < ultimaPos; i++){
+            if (arreglo[i] == 0) {
+                ultimaPos--;
+                for (int j = i; j < ultimaPos; j++){
+                    arreglo[j] = arreglo[j + 1];
+                }
+                arreglo[ultimaPos] = 0;
+                i--;
+            }
+        }
+    }
+
+    public static void rellenaMatriz(int[][] mat, int min, int max){
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[0].length; j++){
+                mat[i][j] = (int)Math.round(Math.random()*(max - min) + min);
+            }
+        }
+    }
+
+    public static void printMatriz(int[][] mat){
+        for (int[] is : mat) {
+            System.out.println(Arrays.toString(is));
+        }
     }
 }
--- Tema3_bis/Test.java
+++ Tema3_bis/Test.java
@@ -11,8 +11,40 @@ public class Test {
         System.out.println(Arrays.toString(prova));*/
         
         // test duplicados
-        /*int[] prova = {0, 12, 1, 1, 66, 66, 3, 4, 5, 5, 66, 8, 3, 0, 12, 12, 15, 3};// repes: 0, 1, 3, 5, 66
+        /* int[] prova = {0, 12, 1, 1, 66, 66, 3, 4, 5, 5, 66, 8, 3, 0, 12, 12, 15, 3};// repes: 0, 1, 3, 5, 66
         int[] dup = Arreglo.duplicados(prova);
-        System.out.println(Arrays.toString(dup));*/
+        System.out.println(Arrays.toString(dup)); */
+
+        // test comunes
+        /* int[] prova1 = {0, 12, 1, 1, 66, 66, 3, 4, 5, 5, 66, 8, 3, 0, 12, 12, 15, 3};
+        int[] prova2 = {3, 2, 4, 0, 11, 5, 8, 10, 12, 3};
+        int[] output = Arreglo.comunes(prova1, prova2);
+        System.out.println(Arrays.toString(output)); */
+        
+        // test sumaIgual
+        /* int[] prova1 = {0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
+        int[][] output = Arreglo.sumaIgual(prova1, 10);
+        Arreglo.printMatriz(output); */
+
+        // test iguales
+        /* int[] prova1 = {0, 1, 2};
+        int[] prova2 = {0, 2, 2};
+        boolean output = Arreglo.iguales(prova1, prova2);
+        System.out.println(output); */
+
+        // test paresImpares
+        /* int[] prova = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] out = Arreglo.paresImpares(prova);
+        System.out.println(Arrays.toString(out)); */
+        
+        // test moverCeros
+        /* int[] prova = {0, 1, 0, 0, 2, 0, 3, 0, 0, 0};
+        Arreglo.moverCeros(prova);
+        System.out.println(Arrays.toString(prova)); */
+
+        // test rellenaMatriz
+        int[][] mat = new int[10][10];
+        Arreglo.rellenaMatriz(mat, 0, 3);
+        Arreglo.printMatriz(mat);
     }
 }
