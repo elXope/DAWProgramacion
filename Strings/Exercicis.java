d7f604ef73d5135680d13b75f9c36c6020f2bfa9
elXope <errchosep25@gmail.com>
2021-12-15 10:35:55 +0100

    Acabats exercicis 2, 3 i 4

--- Strings/Exercicis.java
+++ Strings/Exercicis.java
@@ -10,6 +10,20 @@ public class Exercicis {
          */
 
         // Ej2
+        /* String cadena1 = Utilidades.leerCadena("Primera cadena: ");
+        String cadena2 = Utilidades.leerCadena("Segunda cadena: ");
+        int resultado = compararIgnoreCase(cadena1, cadena2);
+        resultadoComparacion(resultado); */
+        
+        // Ej3
+       /*  String cadena1 = "OOO0";
+        String cadena2 = "s diaaaas";
+        System.out.println(concat(cadena1, cadena2)); */
+
+        // Ej4
+        String cadena1 = "Buenos dias Juan Luis";
+        String cadena2 = "os di";
+        System.out.println(contiene(cadena1, cadena2));
     }
 
     public static int comparar(String cadena1, String cadena2) {
@@ -20,5 +34,17 @@ public class Exercicis {
         if (diferencia > 0) System.out.println("La primera cadena es mayor que la segunda.");
         else if (diferencia < 0) System.out.println("La segunda cadena es mayor que la primera.");
         else System.out.println("Las dos cadenas son iguales.");
+    }
+
+    public static int compararIgnoreCase(String cadena1, String cadena2) {
+        return cadena1.compareToIgnoreCase(cadena2);
+    }
+
+    public static String concat(String cadena1, String cadena2) {
+        return cadena1.concat(cadena2);
+    }
+
+    public static boolean contiene(String cadena1, String cadena2) {
+        return cadena1.contains(cadena2);
     }
 }
