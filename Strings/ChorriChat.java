bde0c06484677d4f8c377392a60066ea66628410
elXope <errchosep25@gmail.com>
2021-12-22 10:29:27 +0100

    Li he ficat coses de time interruption però no tinc ni idea el assistent m'ho ha posat tot

--- Strings/ChorriChat.java
+++ Strings/ChorriChat.java
@@ -1,6 +1,7 @@
 package Strings;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class ChorriChat {
     final String chorri = "Chorri: ";
@@ -28,12 +29,12 @@ public class ChorriChat {
     String respuestaUsuario = "";
     Scanner lector = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ChorriChat chorri = new ChorriChat();
         chorri.chat();
     }
 
-    public void chat() {
+    public void chat() throws InterruptedException {
         inicializar();
         if (!esperarSaludo()) {
             this.lector.close();
@@ -43,8 +44,69 @@ public class ChorriChat {
         this.lector.close();
     }
 
+    private boolean esperarSaludo() throws InterruptedException {
+        while(true) {
+            leerRespuesta();
+            if (comprobarDespedida()) {
+                despedir();
+                return false;
+            }
+            if (comprobarSaludo()) {
+                saludar();
+                return true;
+            }
+            reprender();
+        }
+    }
+
+    private void conversacion() throws InterruptedException {
+        while(true) {
+            leerRespuesta();
+            if (comprobarDespedida()) {
+                despedir();
+                break;
+            }
+            if (comprobarSaludo()) {
+                saludar();
+                continue;
+            }
+            conversar();
+        }
+    }
+
+    private void leerRespuesta() {
+        System.out.print(this.usuario);
+        this.respuestaUsuario = this.lector.nextLine();
+    }
+
+    private boolean comprobarDespedida() {
+        for (String despedidaUsuario : this.despedidasUsuario) {
+            if (despedidaUsuario.equalsIgnoreCase(this.respuestaUsuario)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean comprobarSaludo() throws InterruptedException {
+        for (String saludoUsuario : this.saludosUsuario) {
+            if (saludoUsuario.equalsIgnoreCase(this.respuestaUsuario)) {
+                return true;
+            }
+        }
+        if (buenasNoches()) {
+            beretinOsborne();
+            return true;
+        }
+        return false;
+    }
+
     private void saludar() {
         System.out.println(this.chorri + this.saludo);
+    }
+
+    private void despedir() {
+        System.out.println(this.chorri + this.despedida);
     }
 
     private void reprender() {
@@ -60,47 +122,60 @@ public class ChorriChat {
         System.out.println(this.intro);
     }
 
-    private void leerRespuesta() {
-        System.out.print(this.usuario);
-        this.respuestaUsuario = this.lector.nextLine();
-    }
-
-    private boolean comprobarDespedida() {
-        for (String despedidaUsuario : this.despedidasUsuario) {
-            if (despedidaUsuario.equalsIgnoreCase(this.respuestaUsuario)) {
-                System.out.println(this.chorri + this.despedida);
-                this.lector.close();
-                return true;
-            }
-        }
+    private boolean buenasNoches() {
+        if (this.respuestaUsuario.equalsIgnoreCase("buenas noches")) return true;
         return false;
     }
 
-    private boolean comprobarSaludo() {
-        for (String saludoUsuario : this.saludosUsuario) {
-            if (saludoUsuario.equalsIgnoreCase(this.respuestaUsuario)) {
-                saludar();
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean esperarSaludo() {
-        while(true) {
-            leerRespuesta();
-            if (comprobarDespedida()) return false;
-            if (comprobarSaludo()) return true;
-            reprender();
-        }
-    }
-
-    private void conversacion() {
-        while(true) {
-            leerRespuesta();
-            if (comprobarDespedida()) break;
-            if (comprobarSaludo()) continue;
-            conversar();
+    private void beretinOsborne() throws InterruptedException {
+        final String[] BUENAS_NOCHES = {
+            "Buenas noches señora",
+            "buenas noCHES SEÑORA",
+            "hasta la vista",
+            "gracias por sus sonrisas",
+            "gracias por sus caricias",
+            "hasta la vista",
+            "para mí no hay fronteras",
+            "y mi sitio es cualquiera",
+            "hastaaaa la vista",
+            "buenas noches señooraaaa",
+            "recuerdos a su señor",
+            "                             ,/*******************/,                            ",
+            "                       /*******,,,,,,,,,,,,,,,,,,,*******/                      ",
+            "                  .*****,,,,,,..                 ..,,,,,,****/                  ",
+            "               *****,,,,.                              ..,,,,****,              ",
+            "            .****,,,............ ....... ....... ............,,,,***            ",
+            "          ****,,,,.............................................,,,,***,         ",
+            "        ,***,,,,.................................................,,,,***.       ",
+            "       ***,,,,.....................................................,,,,***      ",
+            "     ,**,,,,,.......................................................,,,,***.    ",
+            "    ***,,,,,.........................................................,,,,,**,   ",
+            "   ****,,,,,,,,,..,(#*..,,,,,,,,.,,,,,,,.,,,,,,,.,,,,,,.,../#/,..,,,,,,,,,,**.  ",
+            "   ***,,,,,,.&%%%%%%%%%%%%*,,,,,,,,,,,,,,,,,,,,,,,,,,,#%%%%%#%%%%%%&.,,,,,,***  ",
+            "  /**,,,,,,&%#.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.%%%.,,,,,**/ ",
+            "  **,,,,,&&.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.%%,,,,*** ",
+            " ,**,,,,&.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.%,,,,**.",
+            " ***,,,,,,,,,,,,,,,,,..,**.*..,,,,,,,,,,,,,,,,,,,,,,,,,...,,....,,,,,,,,,,,,,**.",
+            " ,**,,,,,,,,,,,,(%%%%#######%%%.,,,,,,,,,,,,,,,,,,.%%%%#######%%%%,,,,,,,,,,,**.",
+            "  **,,,,,,,,,,,,,,,,,,,#########,,,,,,,,,,,,,,,,.,,,,,,,,,#######%.,,,,,,,,,*** ",
+            "  ***,,,,,,,,,,,,,,,,,,,.%###%.,,,,,,,,,,,,,,,,,,,,,,,,,,,,#####(,,,,,,,,,,,*** ",
+            "   ***,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,***  ",
+            "   ,***,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,**.  ",
+            "    ****,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,&%.,,,,,,,,,,,,,,,,,***.   ",
+            "     .**,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.%%%.,,,,,,,,,,,,,,,,,**     ",
+            "       ***,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.,%####%,,,,,,,,,,,,,,,,**/      ",
+            "        .***,,,,,,,,,,,,,,,,,,,,,,,,..../#%#########%.,,,,,,,,,,,,,,,***        ",
+            "          .***,,,,,,,,,,,,,,%&&%%%%%%%%%#%%(,..,,,,,,,,,,,,,,,,,,,,***.         ",
+            "             ****,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,**/            ",
+            "               .****,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,****               ",
+            "                   *****,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,****/                  ",
+            "                       ********,,,,,,,,,,,,,,,,,,,*******,                      ",
+            "                             ./******************/*                             "
+        };
+        for (int i = 0; i < BUENAS_NOCHES.length; i ++) {
+            System.out.println(this.chorri + BUENAS_NOCHES[i]);
+            if (i < 11) TimeUnit.SECONDS.sleep(3);
+            else TimeUnit.MILLISECONDS.sleep(300);
         }
     }
 }
