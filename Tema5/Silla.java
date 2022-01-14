9c94d4748ab2448ce2e88055b98907eca2bf9eb9
elXope <errchosep25@gmail.com>
2022-01-14 10:37:55 +0100

    Exercici de classe

--- Tema5/Silla.java
+++ Tema5/Silla.java
@@ -5,5 +5,6 @@ public class Silla {
     No ejecutable.
     Atributos: color, num patas, respaldo o no, nivel de comodidad (0 - 10), material, coordenadas en la habitacion.
     Todos los atributos tienen que ser privados, pero se tienen que poder modificar y acceder a su valor.
+    En la classe test creamos 100 sillas con distintas posiciones y materiales.
      */
 }
