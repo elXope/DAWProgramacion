a9eb0e10044047bbdee3f549555cd5d57f88aa08
elXope <errchosep25@gmail.com>
2022-01-14 08:37:42 +0100

    Creada classe Alumno.java

---
+++ Tema5/Alumno.java
@@ -0,0 +1,39 @@
+package Tema5;
+
+public class Alumno {
+
+    private static int totalAlumnos = 0;
+
+    private int id;
+    private String nombre;
+    private String grupo;
+
+    public Alumno(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.grupo = "No asignado";
+        totalAlumnos++;
+    }
+
+    public Alumno(int id, String nombre, String grupo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.grupo = grupo;
+        totalAlumnos++;
+    }
+
+    public void preguntarDuda() {
+        System.out.println(this.nombre + " levanta la mano");
+    }
+
+    public void imprimirInforme() {
+        System.out.println("Total de alumnos: " + totalAlumnos);
+    }
+
+    public void imprimirInformeAlumnos(Alumno[] alumnos) {
+        System.out.println("Nombre\tGrupo");
+        for (Alumno alumno : alumnos) {
+            System.out.println(alumno.nombre + "\t" + alumno.grupo);
+        }
+    }
+}
