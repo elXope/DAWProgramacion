fa5dfa42d4db682f79ead8b0306fcd41d1bf24e1
elXope <errchosep25@gmail.com>
2022-01-28 10:41:54 +0100

    Avanzado ClockProva.java

--- Tema5/ClockProva.java
+++ Tema5/ClockProva.java
@@ -4,13 +4,15 @@ public class ClockProva {
     public static void main(String[] args) {
         ClockDisplay reloj = new ClockDisplay();
         System.out.println(reloj.getTime());
-        reloj.timeTick();
-        System.out.println(reloj.getTime());
-        reloj.setTime(00, 59, 58);
-        System.out.println(reloj.getTime());
-        reloj.timeTick();
-        System.out.println(reloj.getTime());
-        reloj.timeTick();
-        System.out.println(reloj.getTime());
+        for (int i = 0; i < 60; i++) {
+            reloj.timeTick();
+            System.out.println(reloj.getTime());
+        }
+        for (int i = 0; i < 60; i++) {
+            for (int j = 0; j < 60; j++) {
+                reloj.timeTick();
+            }
+            System.out.println(reloj.getTime());
+        }
     }
 }
