3560b6dcaa844b5c188653c85a8797dce3ca4481
elXope <errchosep25@gmail.com>
2022-02-09 08:44:02 +0100

    Acabat exercici MusicOrganizer

---
+++ Tema5/MusicOrganizer.java
@@ -0,0 +1,74 @@
+package Tema5;
+
+import java.util.ArrayList;
+
+/**
+ * A class to hold details of audio files.
+ *
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29
+ */
+public class MusicOrganizer
+{
+    // An ArrayList for storing the file names of music files.
+    private ArrayList<String> files;
+
+    /**
+     * Create a MusicOrganizer
+     */
+    public MusicOrganizer()
+    {
+        files = new ArrayList<>();
+    }
+
+    /**
+     * Add a file to the collection.
+     * @param filename The file to be added.
+     */
+    public void addFile(String filename)
+    {
+        files.add(filename);
+    }
+
+    /**
+     * Return the number of files in the collection.
+     * @return The number of files in the collection.
+     */
+    public int getNumberOfFiles()
+    {
+        return files.size();
+    }
+
+    public void checkIndex (int index) {
+        if (index < 0 || index >= files.size()) {
+            System.out.println("Los indices de la lista van del " + 0 + " al " + (this.files.size() - 1) + ".");
+        }
+    }
+
+    public boolean isValidIndex (int index) {
+        return index >= 0 && index < files.size();
+    }
+
+    /**
+     * List a file from the collection.
+     * @param index The index of the file to be listed.
+     */
+    public void listFile(int index)
+    {
+        if(this.isValidIndex(index)) {
+            String filename = files.get(index);
+            System.out.println(filename);
+        }
+    }
+
+    /**
+     * Remove a file from the collection.
+     * @param index The index of the file to be removed.
+     */
+    public void removeFile(int index)
+    {
+        if(this.isValidIndex(index)) {
+            files.remove(index);
+        }
+    }
+}
---
+++ Tema5/MusicOrganizerTest.java
@@ -0,0 +1,59 @@
+package Tema5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MusicOrganizerTest {
+
+
+    @Test
+    void isValidIndex() {
+        MusicOrganizer test = new MusicOrganizer();
+        test.addFile("Como un vagabundo");
+        test.addFile("Mi gran noche");
+        test.addFile("La santa");
+        assertFalse(test.isValidIndex(-1));
+        assertFalse(test.isValidIndex(3));
+        assertTrue(test.isValidIndex(0));
+        assertTrue(test.isValidIndex(1));
+        assertTrue(test.isValidIndex(2));
+    }
+
+
+    @Test
+    void getNumberOfFiles() {
+        MusicOrganizer test = new MusicOrganizer();
+        assertEquals(0, test.getNumberOfFiles());
+        test.addFile("Como un vagabundo");
+        test.addFile("Mi gran noche");
+        test.addFile("La santa");
+        assertEquals(3, test.getNumberOfFiles());
+    }
+
+    @Test
+    void removeFile() {
+        MusicOrganizer test = new MusicOrganizer();
+        test.removeFile(0);
+        assertEquals(0, test.getNumberOfFiles());
+        test.addFile("Como un vagabundo");
+        test.addFile("Mi gran noche");
+        test.addFile("La santa");
+        test.removeFile(3);
+        assertEquals(3, test.getNumberOfFiles());
+        test.removeFile(2);
+        assertEquals(2, test.getNumberOfFiles());
+    }
+
+    public static void main(String[] args) {
+        MusicOrganizer test = new MusicOrganizer();
+        test.listFile(0);
+        test.addFile("Como un vagabundo");
+        test.addFile("Mi gran noche");
+        test.addFile("La santa");
+        test.listFile(3);
+        test.listFile(0);
+        test.listFile(1);
+        test.listFile(2);
+    }
+}
