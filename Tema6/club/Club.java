f934fc985ebccdb10e9ab31df1ec95cf6db77488
elXope <errchosep25@gmail.com>
2022-03-04 08:22:50 +0100

    Copiades les classes

---
+++ Tema6/club/Club.java
@@ -0,0 +1,38 @@
+package Tema6.club;
+
+/**
+ * Store details of club memberships.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Club
+{
+    // Define any necessary fields here ...
+
+    /**
+     * Constructor for objects of class Club
+     */
+    public Club()
+    {
+        // Initialise any fields here ...
+
+    }
+
+    /**
+     * Add a new member to the club's list of members.
+     * @param member The member object to be added.
+     */
+    public void join(Membership member)
+    {
+    }
+
+    /**
+     * @return The number of members (Membership objects) in
+     *         the club.
+     */
+    public int numberOfMembers()
+    {
+        return 0;
+    }
+}
