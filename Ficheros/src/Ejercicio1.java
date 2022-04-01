09b153ce15881dda00e5320f0cc9e3897a136934
elXope <errchosep25@gmail.com>
2022-04-01 10:41:32 +0200

    Acabat exercici 1 falta exception d errores

--- Ficheros/src/Ejercicio1.java
+++ Ficheros/src/Ejercicio1.java
@@ -8,19 +8,16 @@ public class Ejercicio1 {
         File actualFile = File.listRoots()[0];
         int opcion = 0;
 
-        imprimirDirectorio(actualFile);
-
         while(true) {
-            opcion = pideOpcion(-1101);
-            if (opcion == -1101) {
-                continue;
-            }
+            imprimirDirectorio(actualFile);
+            opcion = pideOpcion();
             if (opcion != -1) {
                 if (opcion < -1 || opcion > actualFile.list().length) {
                     System.out.println("Introduce un numero valido.");
-                } else {
-                    imprimirDirectorio(actualFile);
-                    actualFile = nuevoDirectorio(opcion - 1);
+                } else if (opcion == 0) {
+                    actualFile = actualFile.getParentFile();
+                } else if (actualFile.listFiles()[opcion - 1].isDirectory()){
+                    actualFile = actualFile.listFiles()[opcion - 1];
                 }
             }
             else {
@@ -29,21 +26,37 @@ public class Ejercicio1 {
         }
     }
 
-    private static int pideOpcion(int def) {
-        System.out.println("Introduce el identificador del directorio al que navegar (-1 para salir):  ");
-        try {
-            return Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Introduce un entero.");
-            return def;
-        } catch (IOException e) {
-            System.out.println("Introduce un parametro valido.");
-            return def;
+    private static int pideOpcion() {
+        while (true) {
+            System.out.println("Introduce el identificador del directorio al que navegar (-1 para salir):  ");
+            try {
+                return Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Introduce un entero.");
+            } catch (IOException e) {
+                System.out.println("Introduce un parametro valido.");
+            }
         }
     }
 
     private static void imprimirDirectorio(File actualFile) {
+        System.out.println("======================================");
         System.out.println("Directorio: " + actualFile.getPath());
+
+        if (actualFile.getParent() != null) {
+            System.out.println(0 + ".-" + actualFile.getParent() + " <Parent>");
+        }
+
+        for (int i = 0; i < actualFile.list().length; i ++) {
+            if (!actualFile.listFiles()[i].isHidden()) {
+                if (actualFile.listFiles()[i].isFile()) {
+                    System.out.println((i+1) + ".-" + actualFile.listFiles()[i].getName() + " " + actualFile.listFiles()[i].getTotalSpace());
+                }
+                else if (actualFile.listFiles()[i].isDirectory()) {
+                    System.out.println((i+1) + ".-" + actualFile.listFiles()[i].getName() + " <Directorio>");
+                }
+            }
+        }
     }
 
     private static File nuevoDirectorio(int opcion) {
