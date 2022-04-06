04eceff9aca6020ea3ecdc84e689ff43286f090b
elXope <errchosep25@gmail.com>
2022-04-06 10:40:23 +0200

    Creat la calsse Mezcla

---
+++ Ficheros/src/Mezcla.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Mezcla {
+    public static void main(String[] args) throws IOException {
+        FileReader f1 = new FileReader("./Ficheros/f1.txt");
+        FileReader f2 = new FileReader("./Ficheros/f2.txt");
+        FileWriter f3 = new FileWriter("./Ficheros/f3.txt");
+
+
+    }
+}
