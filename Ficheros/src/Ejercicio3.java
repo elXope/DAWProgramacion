43e347865d9090e26ae2b047388f7b013298db0e
elXope <errchosep25@gmail.com>
2022-04-11 10:03:18 +0200

    Fent imatges

--- Ficheros/src/Ejercicio3.java
+++ Ficheros/src/Ejercicio3.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 public class Ejercicio3 {
     public static void main(String[] args) throws IOException {
         File img = new File("./Ficheros/penyagolosa.bmp");
-        TransformaImagen imgTrans = new TransformaImagen(img);
-        imgTrans.transformaNegativo();
-        imgTrans.transformaOscuro();
-        imgTrans.transformaBlancoNegro();
+//        TransformaImagen imgTrans = new TransformaImagen(img);
+//        imgTrans.transformaNegativo();
+//        imgTrans.transformaOscuro();
+//        imgTrans.transformaBlancoNegro();
+
+        TransformaImagen2 imagen = new TransformaImagen2(img);
+        imagen.copiaImagen("./Ficheros/penyagolosaCopia.bmp");
+        imagen.transformaNegativo();
     }
 }
--- Ficheros/src/TransformaImagen2.java
+++ Ficheros/src/TransformaImagen2.java
@@ -1,14 +1,13 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class TransformaImagen2 {
     File f = null;
+    BufferedImage image;
 
-    public TransformaImagen2(File fEnt) {
-        // Control de existencia del fichero y control de la extensión .bmp (sacar
-        // mensajes de error)
+    public TransformaImagen2(File fEnt) throws IOException {
         if (!fEnt.isFile()) {
             System.out.println("No es un archivo valido.");
         } else {
@@ -17,73 +16,34 @@ public class TransformaImagen2 {
                 System.out.println("No es un formato valido. Tiene que ser '.bmp'");
             } else {
                 f = fEnt;
+                image = new BufferedImage(525, 393, BufferedImage.TYPE_INT_RGB);
+                image =  ImageIO.read(f);
             }
         }
+    }
+
+    public void copiaImagen(String path) throws IOException {
+        File outputFile = new File(path);
+        ImageIO.write(image, "bmp", outputFile);
     }
 
     public void transformaNegativo() throws IOException {
-        // Transformar a negativo y guardar como *_n.bmp
-        FileInputStream imgIn = new FileInputStream(f);
-        FileOutputStream imgOut = new FileOutputStream(f.getPath().substring(0, f.getPath().length() - 4) + "_n.bmp");
-        for (int i = 0; i < 54; i++) {
-            imgOut.write(imgIn.read());
-        }
-        while (true) {
-            int out = imgIn.read();
-            if (out == -1) {
-                break;
-            } else {
-                imgOut.write(255 - out);
+        BufferedImage neg = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                neg.setRGB(i, j, 255 - image.getRGB(i, j));
             }
         }
-        imgIn.close();
-        imgOut.close();
+
+        File output = new File("./Ficheros/penyagolosa_n2.bmp");
+        ImageIO.write(neg, "bmp", output);
     }
 
     public void transformaOscuro() throws IOException {
-        // Transformar a una imagen más oscura y guardar como *_o.bmp
-        FileInputStream imgIn = new FileInputStream(f);
-        FileOutputStream imgOut = new FileOutputStream(f.getPath().substring(0, f.getPath().length() - 4) + "_o.bmp");
-        for (int i = 0; i < 54; i++) {
-            imgOut.write(imgIn.read());
-        }
-        while (true) {
-            int out = imgIn.read();
-            if (out == -1) {
-                break;
-            } else {
-                imgOut.write(out/2);
-            }
-        }
-        imgIn.close();
-        imgOut.close();
+
     }
 
 
     public void transformaBlancoNegro() throws IOException {
-        // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
-        FileInputStream imgIn = new FileInputStream(f);
-        FileOutputStream imgOut = new FileOutputStream(f.getPath().substring(0, f.getPath().length() - 4) + "_bn.bmp");
-        for (int i = 0; i < 54; i++) {
-            imgOut.write(imgIn.read());
-        }
-
-        int r;
-        int g;
-        int b;
-        while (true) {
-            r = imgIn.read();
-            if (r == -1) {
-                break;
-            } else {
-                g = imgIn.read();
-                b = imgIn.read();
-                imgOut.write((r+g+b)/3);
-                imgOut.write((r+g+b)/3);
-                imgOut.write((r+g+b)/3);
-            }
-        }
-        imgIn.close();
-        imgOut.close();
     }
 }
