808c64218ce356e06345a659c256a397e9229732
elXope <errchosep25@gmail.com>
2022-05-04 10:31:41 +0200

    Acabat escriure i llegir xml i json

--- ControlTema6/src/PersistenciaCliente.java
+++ ControlTema6/src/PersistenciaCliente.java
@@ -1,5 +1,22 @@
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class PersistenciaCliente {
@@ -45,5 +62,86 @@ public class PersistenciaCliente {
 
 
         return resultado;
+    }
+
+    public static void writeXML(ArrayList<Cliente> clientes) throws ParserConfigurationException, TransformerException, FileNotFoundException {
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        Element root = doc.createElement("clientes");
+        doc.appendChild(root);
+
+        for (Cliente cliente : clientes) {
+            Element cli = doc.createElement("cliente");
+
+            Element nif = doc.createElement("nif");
+            nif.appendChild(doc.createTextNode(cliente.getNif()));
+            cli.appendChild(nif);
+
+            Element nombre = doc.createElement("nombre");
+            nombre.appendChild(doc.createTextNode(cliente.getNombre()));
+            cli.appendChild(nombre);
+
+            Element apellidos = doc.createElement("apellidos");
+            apellidos.appendChild(doc.createTextNode(cliente.getApellidos()));
+            cli.appendChild(apellidos);
+
+            Element id = doc.createElement("id");
+            id.appendChild(doc.createTextNode(cliente.getId()));
+            cli.appendChild(id);
+
+            Element email = doc.createElement("email");
+            email.appendChild(doc.createTextNode(cliente.getEmail()));
+            cli.appendChild(email);
+
+            root.appendChild(cli);
+        }
+
+        Transformer trans = TransformerFactory.newInstance().newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new FileOutputStream("./Clientes.xml"));
+
+        trans.transform(source, result);
+    }
+
+    public static ArrayList<Cliente> readXML() throws ParserConfigurationException, IOException, SAXException {
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("./Clientes.xml");
+        Element root = doc.getDocumentElement();
+        NodeList hijos = root.getChildNodes();
+
+        for (int i = 0; i < hijos.getLength(); i++) {
+            NodeList hojas = hijos.item(i).getChildNodes();
+            clientes.add(new Cliente(hojas.item(0).getTextContent(),
+                    hojas.item(1).getTextContent(),
+                    hojas.item(2).getTextContent(),
+                    hojas.item(3).getTextContent(),
+                    hojas.item(4).getTextContent()));
+        }
+
+        return clientes;
+    }
+
+    public static void writeJSON(ArrayList<Cliente> clientes) throws IOException {
+        Gson gson = new Gson();
+        FileWriter destino = new FileWriter("./ClientesJSON.json");
+        BufferedWriter escritor = new BufferedWriter(destino);
+
+        escritor.write("[");
+        for (int i = 0; i < clientes.size() - 1; i++) {
+            escritor.write(gson.toJson(clientes.get(i)) + ",");
+        }
+        escritor.write(gson.toJson(clientes.get(clientes.size() - 1)) + "]");
+
+        escritor.close();
+        destino.close();
+    }
+
+    public static ArrayList<Cliente> readJSON() throws IOException {
+        Type tipoListaClientes = new TypeToken<ArrayList<Cliente>>(){}.getType();
+        Gson gson = new Gson();
+        FileReader fileReader = new FileReader("./ClientesJSON.json");
+        BufferedReader lector = new BufferedReader(fileReader);
+        String elJson = lector.readLine();
+
+        return gson.fromJson(elJson, tipoListaClientes);
     }
 }
--- ControlTema6/src/PersistenciaClienteTest.java
+++ ControlTema6/src/PersistenciaClienteTest.java
@@ -1,3 +1,9 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PersistenciaClienteTest {
@@ -12,13 +18,37 @@ public class PersistenciaClienteTest {
         clientes.add(c2);
         clientes.add(c3);
 
-        PersistenciaCliente.write(clientes);
+//        PersistenciaCliente.write(clientes);
+//
+//        ArrayList<Cliente> clientesRecuperado = PersistenciaCliente.read();
+//        for (int i = 0; i < clientesRecuperado.size(); i++) {
+//            System.out.println(clientesRecuperado.get(i));
+//            System.out.println(clientes.get(i));
+//            System.out.println("==============");
+//        }
 
-        ArrayList<Cliente> clientesRecuperado = PersistenciaCliente.read();
-        for (int i = 0; i < clientesRecuperado.size(); i++) {
-            System.out.println(clientesRecuperado.get(i));
-            System.out.println(clientes.get(i));
-            System.out.println("==============");
+//        try {
+//            PersistenciaCliente.writeXML(clientes);
+//            ArrayList<Cliente> clientesRecuperado = PersistenciaCliente.readXML();
+//            for (int i = 0; i < clientesRecuperado.size(); i++) {
+//                System.out.println(clientesRecuperado.get(i));
+//                System.out.println(clientes.get(i));
+//                System.out.println("==============");
+//            }
+//        } catch (ParserConfigurationException | TransformerException | IOException | SAXException e) {
+//            e.printStackTrace();
+//        }
+
+        try {
+            PersistenciaCliente.writeJSON(clientes);
+            ArrayList<Cliente> clientesRecuperado = PersistenciaCliente.readJSON();
+            for (int i = 0; i < clientesRecuperado.size(); i++) {
+                System.out.println(clientesRecuperado.get(i));
+                System.out.println(clientes.get(i));
+                System.out.println("==============");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
