103ac444e0ecab4444d0b46cbe7b1c7cd8da8e83
elXope <errchosep25@gmail.com>
2022-06-03 10:37:12 +0200

    ole

--- Bdd/src/TestDB.java
+++ Bdd/src/TestDB.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class TestDB {
 
     public static void main(String[] args) {
-        String consulta = "SELECT count(*) as total FROM Cliente";
+        String consulta = "insert into Clientes values (null, '56473465J', 'Juan', 'Heredia Molinos', 'lol.lol@gmail.com')";
         String connectionUrl = "jdbc:mysql://192.168.204.160:3306/clientes";
 
         try {
@@ -15,17 +15,24 @@ public class TestDB {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
-
-        try (Connection conn = DriverManager.getConnection(connectionUrl, "phpmyadmin", "phpmyadmin");
-             PreparedStatement ps = conn.prepareStatement(consulta);
-             ResultSet rs = ps.executeQuery()) {
-
-            while (rs.next()) {
-                int total = rs.getInt("total");
-                System.out.println("Total: " + total);
-            }
+        try (Connection conn = DriverManager.getConnection(connectionUrl, "phpmyadmin", "phpmyadmin")) {
+            PreparedStatement ps = conn.prepareStatement(consulta);
+            ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
+//        try (Connection conn = DriverManager.getConnection(connectionUrl, "phpmyadmin", "phpmyadmin");
+//             PreparedStatement ps = conn.prepareStatement(consulta);
+//             ResultSet rs = ps.executeQuery()) {
+//
+//            while (rs.next()) {
+//                int total = rs.getInt("total");
+//                System.out.println("Total: " + total);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
